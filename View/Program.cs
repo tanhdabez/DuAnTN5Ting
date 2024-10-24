7d87bdbcd9513bf2c87c9f7a5e74ecbd3b57e5c3
huytran242 <tranuochuy2402@gmail.com>
2024-10-24 19:02:22 +0700

    View Khach

--- View/Program.cs
+++ View/Program.cs
@@ -33,7 +33,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=UI}/{action=Index}/{id?}");
 
 app.UseStatusCodePagesWithReExecute("/Error/{0}");
 
