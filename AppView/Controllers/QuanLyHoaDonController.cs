24aef89cd7f6ac48ff8166ce3a5b5e942e23f60d
Minh Mạnh <tranuochuy2402@gmail.com>
2024-11-29 23:31:17 +0700

    fix controller qlhd

--- AppView/Controllers/QuanLyHoaDonController.cs
+++ AppView/Controllers/QuanLyHoaDonController.cs
@@ -30,7 +30,98 @@ namespace AppView.Controllers
             return View();
         }
         // Load tất cả hóa đơn
-        
+        public async Task<IActionResult> LoadAllHoaDon(FilterHD filter)
+        {
+            var listhdql = await _httpClient.GetFromJsonAsync<List<HoaDonQL>>("HoaDon/GetAllHDQly");
+            listhdql = listhdql.OrderByDescending(c => c.ThoiGian).ToList();
+            //Lọc thời gian
+            if (filter.ngaybd != null)
+            {
+                string[] formats = { "MM/dd/yyyy HH:mm:ss" };
+                DateTime parsedDate = DateTime.ParseExact(filter.ngaybd, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+                DateTime parsedDate1 = DateTime.ParseExact(filter.ngaykt, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+                string output = parsedDate.ToString("MM/dd/yyyy HH:mm:ss");
+                string output1 = parsedDate1.ToString("MM/dd/yyyy HH:mm:ss");
+                var bd = DateTime.ParseExact(output, formats, new CultureInfo("en-US"), DateTimeStyles.None);
+                var kt = DateTime.ParseExact(output1, formats, new CultureInfo("en-US"), DateTimeStyles.None);
+                listhdql = listhdql.Where(c => c.ThoiGian >= bd && c.ThoiGian <= kt).ToList();
+            }
+            //Tìm kiếm 
+            if (filter.keyWord != null)
+            {
+                if (filter.loaitk == 1)
+                {
+                    listhdql = listhdql.Where(c => c.MaHD.ToLower().Contains(filter.keyWord.Trim().ToLower()) || (c.SDTnhanhang != null && c.SDTnhanhang.Contains(filter.keyWord.Trim()))).ToList();
+                }
+                else if (filter.loaitk == 2)
+                {
+                    listhdql = listhdql.Where(c => c.KhachHang.ToLower().Contains(filter.keyWord.Trim().ToLower()) || (c.SDTKH != null && c.SDTKH.Contains(filter.keyWord.Trim()))).ToList();
+                }
+            }
+
+            //Lọc kênh
+            if (filter.loaiHD != null)
+            {
+                listhdql = listhdql.Where(c => filter.loaiHD.Contains(c.LoaiHD)).ToList();
+            }
+            //Lọc trạng thái
+            if (filter.lstTT != null)
+            {
+                listhdql = listhdql.Where(c => filter.lstTT.Contains(c.TrangThai)).ToList();
+            }
+
+            // Tổng tiền hàng
+            var tth = listhdql.Sum(c => c.TongTienHang);
+            // Tổng tiền khách đã trả
+            var tktra = listhdql.Sum(c => c.KhachDaTra);
+
+            int totalRow = listhdql.Count;
+            var model = listhdql.Skip((filter.page - 1) * filter.pageSize).Take(filter.pageSize).ToArray();
+            //Lọc loại hd
+            return Json(new
+            {
+                tienhang = tth,
+                khachtra = tktra,
+                data = model,
+                total = totalRow,
+                status = true,
+            });
+        }
+
+        //Chi tiết hóa đơn 
+        [HttpGet("/QuanLyHoaDon/ViewChiTietHD/{idhd}")]
+        public async Task<IActionResult> ViewChiTietHD(string idhd)
+        {
+            var hd = await _httpClient.GetFromJsonAsync<ChiTietHoaDonQL>($"HoaDon/ChiTietHoaDonQL/{idhd}");
+            return PartialView("_ThongTinHD", hd);
+        }
+        //Sao chép hóa đơn
+        [HttpGet("/QuanLyHoaDon/CopyHD")]
+        public async Task<IActionResult> CopyHD(string idhd)
+        {
+            try
+            {
+                var loginInfor = new LoginViewModel();
+                string? session = HttpContext.Session.GetString("LoginInfor");
+                if (session != null)
+                {
+                    loginInfor = JsonConvert.DeserializeObject<LoginViewModel>(session);
+                }
+                var idnv = loginInfor.Id;
+
+                string url = $"HoaDon/CopyHD?idhd={idhd}&idnv={idnv}";
+                var response = await _httpClient.PutAsync(url, null);
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("BanHang", "BanHangTaiQuay");
+                }
+                return Json(new { success = false, message = "Sao chép hóa đơn thất bại" });
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("_QuanLyHoaDon", "QuanLyHoaDon");
+            }
+        }
         // Cập nhật trạng thái
         public async Task<IActionResult> DoiTrangThai(Guid idhd, int trangthai)// Dùng cho trạng thái truyền  vào: 10, 3
         {
